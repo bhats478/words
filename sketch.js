035e515ee2e5aab36eeba494f4aa50bdcac8a9de
bhats478 <bhats478@newschool.edu>
2016-04-17 17:26:55 -0400

    Finished assignment

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
-//Feel free to change these words:
-var nouns = ["Hammer","Cat","Tree", "Silence", "Umbrella","Sofa","Shirt","Jacket"];
 
+var nouns = ["banana","bottle","candle","sweater","purse","sock","foot"];
+var randomNoun;
+var be = ["is","was","became","looks"]
+var randomBe;
+var adjective = ["red","orange","yellow","green","blue"]
+var randomAdjective;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	
+
+	var nounIndex = floor(random(nouns.length));
+	randomNoun = nouns[nounIndex];
+	var beIndex = floor(random(be.length));
+	randomBe = be[beIndex];
+	var adjectiveIndex = floor(random(adjective.length));
+	randomAdjective = adjective[adjectiveIndex];
+
 }
-
 function draw(){
-	background("silver");
+	background(randomAdjective);
+	textSize(40);
 
+	fill("black");
+	text(randomNoun, 200, height/2);
+	text(randomBe, 400, height/2);
+	text("the",100,height/2);
+	text(randomAdjective,600, height/2);
 }
